65cba9ec40a126911ba221825d07090520589124
Pablo Bernal <pj.bernal05@outlook.com>
2024-05-12 18:53:15 -0500

    Third part complete with formatting.
    Review the code once again to format adequately the sub-breeds

--- challenge02_three.rb
+++ challenge02_three.rb
@@ -7,6 +7,15 @@ uri = URI(url)
 response = Net::HTTP.get(uri)
 dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
 ## pp dog_breeds # pp stands for pretty print.
-dog_breeds.each do |dogs|
-  pp dogs
+dog_breeds.each do |message, dog_hash|
+  if dog_hash.is_a?(Hash)
+
+    dog_hash.each do |dog, breeds|
+      puts "- #{dog.capitalize} has #{breeds.length()} sub-breeds"
+
+        breeds.each_index do |i|
+          puts "  - #{i + 1}. #{breeds[i].capitalize}"
+        end
+    end
+  end
 end
