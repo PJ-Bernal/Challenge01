b3bd2d6edc68d2fc22ee2e3d0b0f13801764b5a6
Pablo Bernal <pj.bernal05@outlook.com>
2024-05-12 16:30:56 -0500

    Carl and part two finish. Added pp functionality. Assignment complete

--- challenge02_three.rb
+++ challenge02_three.rb
@@ -1,1 +1,12 @@
-puts("this isn't important") && puts("THIS IS IMPORTANT!!!")
+require 'net/http'
+require 'json'
+require 'pp'
+
+url = 'https://dog.ceo/api/breeds/list/all'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
+## pp dog_breeds # pp stands for pretty print.
+dog_breeds.each do |dogs|
+  pp dogs
+end
